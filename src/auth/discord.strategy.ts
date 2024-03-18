359701c9a3833accac52e6fa09625543ea12d21e
lotosdb <riderandresj@hotmail.com>
2024-03-17 20:35:39 -0500

    test uri discord

--- src/auth/discord.strategy.ts
+++ src/auth/discord.strategy.ts
@@ -9,7 +9,7 @@ import { HttpService } from '@nestjs/axios';
 // change these to be your Discord client ID and secret
 const clientID = '1216925942572650566'; //953826763094499328
 const clientSecret = '6uPb_GC3eyI__UOsM9pulxQU4ezh-OUF'; //axC7kdZN4kx3toAz491C2LVipGd_n17S
-const callbackURL = 'https://codequest2024front.onrender.com/principal';
+const callbackURL = 'http://localhost:5173/principal'; //'https://codequest2024front.onrender.com/principal';
 
 @Injectable()
 export class DiscordStrategy extends PassportStrategy(Strategy, 'discord') {
